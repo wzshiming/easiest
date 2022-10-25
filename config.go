3ca414b812827f629044eb0f4f1edad5180616ab
Shiming Zhang <wzshiming@foxmail.com>
2022-10-25 21:42:23 +0800

    Update

--- config.go
+++ config.go
@@ -1,21 +1,13 @@
 package easiest
 
-import (
-	"crypto/tls"
-	"fmt"
-	"net"
-	"net/url"
-)
-
 type Config struct {
 	TlsDir string  `yaml:"tlsDir,omitempty"`
 	Routes []Route `yaml:"routes,omitempty"`
 }
 
 type HttpConfig struct {
-	ForceTLS           bool    `yaml:"forceTLS,omitempty"`
-	HeaderForwardedFor bool    `yaml:"headerForwardedFor,omitempty"`
-	AcceptEncoding     *string `yaml:"acceptEncoding,omitempty"`
+	ForceTLS           bool `yaml:"forceTLS,omitempty"`
+	HeaderForwardedFor bool `yaml:"headerForwardedFor,omitempty"`
 }
 
 type Route struct {
@@ -23,33 +15,10 @@ type Route struct {
 	Target   string     `yaml:"target,omitempty"`
 	HTTP     HttpConfig `yaml:"http,omitempty"`
 	Replaces []Replace  `yaml:"replaces,omitempty"`
+	Stream   bool       `yaml:"stream,omitempty"`
 }
 
 type Replace struct {
 	Old string `yaml:"old,omitempty"`
 	New string `yaml:"new,omitempty"`
-}
-
-func dialTarget(o string) (net.Conn, error) {
-	u, err := url.Parse(o)
-	if err != nil {
-		return nil, err
-	}
-	switch u.Scheme {
-	case "http":
-		port := u.Port()
-		if port == "" {
-			port = "80"
-		}
-		return net.Dial("tcp", net.JoinHostPort(u.Hostname(), port))
-	case "https":
-		port := u.Port()
-		if port == "" {
-			port = "443"
-		}
-		return tls.Dial("tcp", net.JoinHostPort(u.Hostname(), port), &tls.Config{
-			InsecureSkipVerify: true,
-		})
-	}
-	return nil, fmt.Errorf("unsupported scheme %q", u.Scheme)
 }
